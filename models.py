e92274df127110ff4b289390f46dd4ae02fa6e4b
EmersonGrace_Brown <gsz5cv@virginia.edu>
2025-10-01 21:44:09 -0400

    Implement␣Pydantic␣extensions␣and␣hashing

--- models.py
+++ models.py
@@ -9,6 +9,8 @@ class SurveySubmission(BaseModel):
     consent: bool = Field(..., description="Must be true to accept")
     rating: int = Field(..., ge=1, le=5)
     comments: Optional[str] = Field(None, max_length=1000)
+    user_agent: Optional[str] = None
+    submission_id: Optional[str] = None
   
 
     @validator("comments")
@@ -23,5 +25,15 @@ class SurveySubmission(BaseModel):
         
 #Good example of inheritance
 class StoredSurveyRecord(SurveySubmission):
+    name: str
+    consent: bool
+    rating: int
+    comments: Optional[str] = None
+    user_agent: Optional[str] = None
+
+    hashed_email: str
+    hashed_age: str
+    submission_id: str
+    
     received_at: datetime
     ip: str
